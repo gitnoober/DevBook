a8ee4cd8c5eecaa4dbcb3e9834fc4e097b6adfd9
gitnoober <priyanshusingh349@gmail.com>
2022-02-18 18:23:38 +0530

    added post,likes,comments routes

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>
+);
+
+export default App;
